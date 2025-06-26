8a0a19f4d53d372418c37f84764ee1acc77ef2cc
KongHauYiLLL <ihugoland@gmail.com>
2025-06-26 15:50:37 +0800

    Updated Mining.tsx

--- src/components/Mining.tsx
+++ src/components/Mining.tsx
@@ -31,7 +31,7 @@ export const Mining: React.FC<MiningProps> = ({ mining, gems, onMineGem, onPurch
       x: Math.floor(Math.random() * GRID_SIZE),
       y: Math.floor(Math.random() * GRID_SIZE),
       clicks: 0,
-      maxClicks: 15,
+      maxClicks: 1,
       id: Math.random().toString(36).substr(2, 9),
     };
     return newNode;
